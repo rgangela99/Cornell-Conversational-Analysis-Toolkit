6ececc2ce6efe3e8e7369aa58d54aa07ee512e08
calebchiam <calebchiam@gmail.com>
2020-08-20 23:02:42 +0800

    c

--- convokit/tsne/tsneTransformer.py
+++
@@ -1,9 +0,0 @@
-from convokit import Transformer, Corpus
-
-
-class TSNETransformer(Transformer):
-    def __init__(self):
-        pass
-
-    def transform(self, corpus: Corpus, **kwargs) -> Corpus:
-        pass
