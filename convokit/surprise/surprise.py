4cae788b0a63addb937b9ca5e8a006eeb9ef4c6f
rgangela99 <rgangela99@gmail.com>
2020-09-27 01:14:32 -0400

    parameterized transformer v1

--- convokit/surprise/surprise.py
+++ convokit/surprise/surprise.py
@@ -1,17 +1,20 @@
 import numpy as np
+import pandas as pd
 from collections import defaultdict
 from convokit import Transformer
-from convokit.model import Corpus, CorpusComponent
+from convokit.model import Corpus, CorpusComponent, Utterance
 from convokit.speaker_convo_helpers import SpeakerConvoAttrs
 from itertools import chain
 from math import log2
 from numpy import inf
+from pandas.core.groupby.generic import SeriesGroupBy
 from sklearn.feature_extraction.text import CountVectorizer
-from typing import Callable, List
+from typing import Callable, List, Tuple, Union
 
 def _cross_entropy(model, target, context):
   # H(P,Q) = -sum_{x\inX}(P(x) * log(Q(x)))
   N_target, N_context = target.sum(), context.sum()
+  if N_context == 0: return np.nan
   context_log_probs = np.log2(context / N_context)
   # log(0) gives us -inf, so replace all -inf with 0
   context_log_probs[context_log_probs == -inf] = 0 
@@ -26,8 +29,19 @@ class Surprise(Transformer):
   """
   TODO: Docs
   """
-  def __init__(self, min_target_length=100, min_context_length=100, n_samples=50, 
+  df_col_name = {
+    'speaker': 'speaker',
+    'conversation': 'conversation_id'
+  }
+
+  obj_field_getter = {
+    'speaker': lambda obj: obj.get_speaker().id,
+    'conversation_id': lambda obj: obj.get_conversation().id
+  }
+
+  def __init__(self, cv=CountVectorizer(), min_target_length=100, min_context_length=100, n_samples=50, 
       sampling_fn: Callable[[List[str], int], List[str]]=sample, surprise_attr_name="surprise"):
+    self.cv = cv
     self.min_target_length = min_target_length
     self.min_context_length = min_context_length
     self.n_samples = n_samples
@@ -35,69 +49,51 @@ class Surprise(Transformer):
     self.surprise_attr_name = surprise_attr_name
   
   def fit(self, corpus: Corpus, 
-      target_selector: Callable[[CorpusComponent], bool]=lambda _: True, 
-      context_selector: Callable[[CorpusComponent], bool]=lambda _:True,
-      context_groupby: List[str]=[]):
-
-    speaker_convo_utterances = defaultdict(lambda: defaultdict(list))
-    for speaker in corpus.iter_objs('speaker'):
-      for utt in speaker.iter_utterances():
-        if utt.text:
-          speaker_convo_utterances[speaker.id][utt.conversation_id].append(utt.text)
-
-    self.grouped_text = {
-      speaker: {
-        convo: ' '.join(convo_utterances) for convo, convo_utterances in convos.items()
-      } for speaker, convos in speaker_convo_utterances.items()
-    }
-
-    models = defaultdict(dict)
-    tokenizers = defaultdict(dict)
-    for speaker, convos in self.grouped_text.items():
-      speaker_convos = []
-      for convo_id, convo_text in convos.items():
-        if convo_text:
-          speaker_convos.append(convo_text)
-      try:
-        cv = CountVectorizer()
-        cv.fit(speaker_convos)
-        models[speaker] = cv
-        tokenizers[speaker] = cv.build_analyzer()
-      except ValueError:
-        continue
-    self.mapped_models = models
-    self.grouped_tokens = {
-      speaker: {
-        convo: tokenizers[speaker](text) for convo, text in convos.items()
-      } for speaker, convos in self.grouped_text.items()
-    }
+      group_models_by: List[str]=[],
+      selector: Callable[[Utterance], bool]=lambda utt: True):
+    utterances = corpus.get_utterances_dataframe()
+    if group_models_by:
+      model_groups = utterances.groupby([self.df_col_name[group] for group in group_models_by])
+      self.mapped_models = model_groups['text'].apply(self.fit_cv).dropna()
+    else:
+      self.mapped_models = pd.Series(self.cv.fit(utterances['text']))
     return self
+
+  def fit_cv(self, text):
+    try:
+      cv = CountVectorizer().set_params(**self.cv.get_params())
+      cv.fit(text)
+      return cv
+    except ValueError:
+      return None
 
   def transform(self, corpus: Corpus,
       obj_type: str,
+      group_target_by: List[str] = [],
+      context_selector: Callable[[pd.Series, Tuple], np.ndarray]=lambda s, t: np.ones(len(s)).astype(bool), 
+      model_selector: Callable[[Tuple], Union[str, int]]=lambda _: 0,
       selector: Callable[[CorpusComponent], bool]=lambda _: True):
-    count = 0
-    seen = defaultdict(dict)
+    utterances = corpus.get_utterances_dataframe()
+    if group_target_by:
+      grouped_utterances = utterances.groupby([self.df_col_name[group] for group in group_target_by])['text'].apply(lambda x: self.cv.build_analyzer()(' '.join(x)))
+    else:
+      grouped_utterances = pd.Series(self.cv.build_analyzer()(' '.join(utterances['text'])))
+    surprise_scores = {}
+    for ind, val in grouped_utterances.items():
+      model_ind = model_selector(ind)
+      if model_ind in self.mapped_models:
+        model = self.mapped_models[model_ind]
+        target = val
+        context = list(chain(*grouped_utterances[context_selector(grouped_utterances, ind)]))
+        surprise_scores[ind] = self.compute_surprise(model, target, context)
+      else:
+        surprise_scores[ind] = np.nan
     for obj in corpus.iter_objs(obj_type):
       if selector(obj):
-        speaker_id = obj.get_speaker().get_id()
-        convo_id = obj.get_conversation().get_id()
-        if speaker_id in seen and convo_id in seen[speaker_id]:
-          obj.add_meta(self.surprise_attr_name, seen[speaker_id][convo_id])
-        elif speaker_id in self.mapped_models and convo_id in self.grouped_text[speaker_id]:
-          speaker_model = self.mapped_models[speaker_id]
-          target_toks = self.grouped_tokens[speaker_id][convo_id]
-          context_toks = list(chain(*[toks for convo, toks in self.grouped_tokens[speaker_id].items() if convo != convo_id]))
-          surprise_score = self.compute_surprise(speaker_model, target_toks, context_toks)
-          obj.add_meta(self.surprise_attr_name, surprise_score)
-          seen[speaker_id][convo_id] = surprise_score
-        else:
-          obj.add_meta(self.surprise_attr_name, None)
-      else:
-        obj.add_meta(self.surprise_attr_name, None)
-      count += 1
-      if count % 2500 == 0:
-        print('transformed: {} utterances'.format(count))
+        ind = tuple(map(lambda x: self.obj_field_getter[x](obj), grouped_utterances.index.names))
+        if len(ind) == 1:
+          ind = ind[0]
+        obj.add_meta(self.surprise_attr_name, surprise_scores[ind])
     return corpus
 
   def compute_surprise(self, model: CountVectorizer, target: List[str], context: List[str]):
